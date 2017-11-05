ae378527afdb4d430cc4db3649839eaab599facb
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2017-11-06 01:18:16 +0900

    Fix an ordering

--- app/decorators/board_decorator.rb
+++ app/decorators/board_decorator.rb
@@ -3,7 +3,7 @@ module BoardDecorator
     {
         id: id,
         title: title,
-        items: items.map { |item|
+        items: items.order(:position).map { |item|
           {
               title: item.title,
               stickers: item.stickers.select(:id, :x, :y)
